8ca26a991e06771d05725a12bda6472138c41c81
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-03-28 02:05:11 +0500

    Navbar

--- gym_excercises/src/Pages/Home.jsx
+++ gym_excercises/src/Pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExcercises from "../components/SearchExcercises";
+import Excercises from "../components/Excercises";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box>
+      <HeroBanner />
+      <SearchExcercises />
+      <Excercises />
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
