604e920d3e72092f98c183bf96b473506bb45686
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-03-28 01:40:07 +0500

    project structure

--- gym_excercises/src/App.js
+++ gym_excercises/src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import {Route,Routes} from'react-router-dom'
-import{Box} from '@mui/material'
-import'./App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import ExcerciseDetail from "./Pages/ExcerciseDetail";
+import Footer from "./components/Footer";
+import "./App.css";
 
 const App = () => {
   return (
-    <Box width='400px'>
-
-<Navber/>
-<Routes>
-  <Route path='/' element={<Home />} />
-  <Route path='/excercise:id' element={<ExcerciseDetail />} />
-
-</Routes>
-
-
+    <Box width="400px" sx={{width:{xl:'1488px'}} } m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/excercise:id" element={<ExcerciseDetail />} />
+      </Routes>
+      <Footer />
     </Box>
-  )
-}
+  );
+};
 
-export default App
+export default App;
