81abe940745a988ac15842286f805a16ef09178f
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-03 01:53:47 +0500

    production environment completed

--- gym_excercises/src/utils/fetchData.js
+++ gym_excercises/src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': 'c475bef4e7mshae95e3e88e98c6ap168c0cjsnfd441f0b6a29',
+    'X-RapidAPI-Key': process.env.Rapid_API_Key,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -10,7 +10,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'b4fd7b8f62msh2d364260a33bbc1p181840jsn8990a4feaf76',
+    'X-RapidAPI-Key': process.env.Youtube_API_Key,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
