9a4ae6d770e8ff911b33bccdc3d0c6bc9dc4067f
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-14 03:23:18 +0500

    linked to netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f2 ">
       <Stack gap="40px " alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px" />
         <Typography variant="h5" pb="40px" mt="20px">
           Exercise to live more 💪
         </Typography>
